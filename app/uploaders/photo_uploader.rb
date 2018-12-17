c9fba6f6400df04097d01e5c854b9d4e2986682e
kevin robert <kevin.j.robert@gmail.com>
2018-12-17 08:36:52 +0100

    configure uploader

---
+++ app/uploaders/photo_uploader.rb
@@ -0,0 +1,11 @@
+class PhotoUploader < CarrierWave::Uploader::Base
+  include Cloudinary::CarrierWave
+
+  process eager: true  # Force version generation at upload time.
+
+  process convert: 'jpg'
+
+  version :thumbnail do
+    resize_to_fit 400, 400
+  end
+end
